16f4aaa542a438a8066d421ed1809e2c6d8ef384
Alejandro Rivero Garcia <arivero-@c1r1s2.42madrid.com>
2023-04-12 16:02:49 +0200

    Minor changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: arivero- <arivero-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 13:12:10 by arivero-          #+#    #+#             */
-/*   Updated: 2023/04/11 15:35:34 by arivero-         ###   ########.fr       */
+/*   Updated: 2023/04/12 15:59:37 by arivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,22 @@
 
 char *get_next_line(int fd)
 {
-	char	*str;
+	char		*str;
 	static char	buffer[BUFFER_SIZE];
-	size_t	i;
+	size_t		i;
+	int			read_bytes;
+
+	i = 0;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	str = malloc(sizeof(char) * BUFFER_SIZE + 1)
+	str = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if(!str)
+		return (NULL);
+	read_bytes = read(fd, buffer, BUFFER_SIZE);
+	while(read_bytes > 0)
+	{
+
+	}
+	return (str);
 }
